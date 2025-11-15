200ff3543b153c48e7ed9d01c94b255268f24209
janmalvinescasinas-lab <janmalvinescasinas@gmail.com>
2025-11-15 09:17:43 +0800

    initial commit

---
+++ Activity cajolo.cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int i = 0;
+		while (i < 20)
+		{
+			if(i % 2 == 0)
+			{
+			Console.WriteLine(i);
+			}
+		}
+	}
+}
